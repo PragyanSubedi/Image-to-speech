dc5e94e6ce57827ff82377464a62156061866715
Pragyan Subedi <pragyansubedi@gmail.com>
2018-01-13 23:09:08 +0545

    Added customized directory for traineddata

--- imagetotext.py
+++ imagetotext.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import pytesseract
-
+tessdata_dir_config = '--tessdata-dir "/home/pragyan/PycharmProjects/Imagetospeech/tessdata"'
 im = Image.open("testimages/nepalitest1.png")
 
-text = pytesseract.image_to_string(im, lang = 'nep')
+text = pytesseract.image_to_string(im, lang = 'nep', config=tessdata_dir_config)
 
 print(text)
