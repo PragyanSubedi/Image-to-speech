d987fb2e571e54029da1e158339cf29399749db1
Pragyan Subedi <pragyansubedi@gmail.com>
2018-01-14 08:57:57 +0545

    Added ubuntu fonts

--- imagetotext.py
+++ imagetotext.py
@@ -3,6 +3,6 @@ import pytesseract
 tessdata_dir_config = '--tessdata-dir "/home/pragyan/PycharmProjects/Imagetospeech/tessdata"'
 im = Image.open("testimages/test3.png")
 
-text = pytesseract.image_to_string(im, lang = 'UbuntuC', config=tessdata_dir_config)
+text = pytesseract.image_to_string(im, lang = 'UbuntuR', config=tessdata_dir_config)
 
 print(text)
